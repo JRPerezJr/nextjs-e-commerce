a428a8af05ef3cfba0fd5fd52452d1824f08247c
Juan <pront23@gmail.com>
2021-09-30 13:35:47 +0900

    added cart and login links

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
+import NextLink from 'next/link';
 
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
@@ -13,7 +20,20 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Fashion Monsta</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Fashion Monsta</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
