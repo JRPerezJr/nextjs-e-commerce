4142925d75b9f10950417efd91f22146f5c6801e
Juan <pront23@gmail.com>
2021-12-08 03:06:36 +0900

    added paymentResult to Schema

--- models/Order.js
+++ models/Order.js
@@ -24,6 +24,11 @@ const orderSchema = new mongoose.Schema(
       country: { type: String, required: true },
     },
     paymentMethod: { type: String, required: true },
+    paymentResult: {
+      id: String,
+      status: String,
+      email_address: String,
+    },
     subtotal: { type: Number, required: true },
     shippingCost: { type: Number, required: true },
     taxPrice: { type: Number, required: true },
