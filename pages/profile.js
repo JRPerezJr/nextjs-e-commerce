74bff04aca3fc9fbbc65620d5aa976d38173ce15
Juan <pront23@gmail.com>
2021-12-09 14:25:12 +0900

    completed profile page

---
+++ pages/profile.js
@@ -0,0 +1,256 @@
+import { useContext, useEffect } from 'react';
+
+import dynamic from 'next/dynamic';
+import router from 'next/router';
+import NextLink from 'next/link';
+
+import Layout from '../components/Layout';
+
+import {
+  Card,
+  Grid,
+  Typography,
+  List,
+  ListItem,
+  TextField,
+  Button,
+  ListItemText,
+} from '@mui/material';
+
+import { StoreContext } from '../utils/Store';
+import useStyles from '../utils/styles';
+import { Controller, useForm } from 'react-hook-form';
+import { useSnackbar } from 'notistack';
+
+const Profile = () => {
+  const { state, dispatch } = useContext(StoreContext);
+  const { userInfo } = state;
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (!userInfo) {
+      return router.push('/login');
+    }
+
+    setValue('name', userInfo.name);
+    setValue('email', userInfo.email);
+  }, []);
+
+  const submitHandler = async ({ name, email, password, confirmPassword }) => {
+    closeSnackbar();
+
+    if (password !== confirmPassword) {
+      enqueueSnackbar('Passwords must match', { variant: 'error' });
+      return;
+    }
+    try {
+      const formData = { name, email, password };
+      const response = await fetch(`/api/users/profile`, {
+        method: 'PUT',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${userInfo.token}`,
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.status !== 200) {
+        return enqueueSnackbar('Profile update error', { variant: 'error' });
+      } else {
+        const data = await response.json();
+        dispatch({ type: 'USER_LOGIN', payload: data });
+        enqueueSnackbar('Profile updated successfully', { variant: 'success' });
+      }
+    } catch (error) {
+      console.log('Backend failure');
+    }
+  };
+
+  return (
+    <Layout title="Profile">
+      <Grid container spacing={1}>
+        <Grid item md={3} xs={12}>
+          <Card className={classes.section}>
+            <List>
+              <NextLink href="/profile" passHref>
+                <ListItem selected button component="a">
+                  <ListItemText primary="User Profile" />
+                </ListItem>
+              </NextLink>
+
+              <NextLink href="/order-history" passHref>
+                <ListItem button component="a">
+                  <ListItemText primary="Order History" />
+                </ListItem>
+              </NextLink>
+            </List>
+          </Card>
+        </Grid>
+
+        <Grid item md={9} xs={12}>
+          <Card className={classes.section}>
+            <List>
+              <ListItem>
+                <Typography component="h1" variant="h1">
+                  Profile
+                </Typography>
+              </ListItem>
+
+              <ListItem>
+                <form
+                  className={classes.form}
+                  onSubmit={handleSubmit(submitHandler)}
+                >
+                  <List>
+                    <ListItem>
+                      <Controller
+                        name="name"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                          minLength: 2,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="name"
+                            label="Name"
+                            inputProps={{ type: 'text' }}
+                            error={Boolean(errors.name)}
+                            helperText={
+                              errors.name
+                                ? errors.name.type === 'pattern'
+                                  ? 'Name must be longer than 1 character'
+                                  : 'Name is required'
+                                : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+
+                    <ListItem>
+                      <Controller
+                        name="email"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                          pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="email"
+                            label="Email"
+                            inputProps={{ type: 'email' }}
+                            error={Boolean(errors.email)}
+                            helperText={
+                              errors.email
+                                ? errors.email.type === 'pattern'
+                                  ? 'Email is not valid'
+                                  : 'Email is required'
+                                : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+
+                    <ListItem>
+                      <Controller
+                        name="password"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          validate: (value) =>
+                            value === '' ||
+                            value.length > 6 ||
+                            'Password length is more than 6',
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="password"
+                            label="Password"
+                            inputProps={{ type: 'password' }}
+                            error={Boolean(errors.password)}
+                            helperText={
+                              errors.password
+                                ? 'Password must be longer than 6 characters'
+                                : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+
+                    <ListItem>
+                      <Controller
+                        name="confirmPassword"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          validate: (value) =>
+                            value === '' ||
+                            value.length > 6 ||
+                            'Password length is more than 6',
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="confirmPassword"
+                            label="Confirm Password"
+                            inputProps={{ type: 'password' }}
+                            error={Boolean(errors.confirmPassword)}
+                            helperText={
+                              errors.password
+                                ? 'Confirm Password must be longer than 6 characters'
+                                : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+
+                    <ListItem>
+                      <Button
+                        variant="contained"
+                        type="submit"
+                        fullWidth
+                        color="primary"
+                      >
+                        Update
+                      </Button>
+                    </ListItem>
+                  </List>
+                </form>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+};
+
+export default dynamic(() => Promise.resolve(Profile), { ssr: false });
